53b4ac9d7e7cb6788e9756ba74131dd5b545ee8b
Anas Lharrak <146816560+anaslharrak@users.noreply.github.com>
2024-07-14 19:00:17 +0200

    refactor: :art: refactor principal page
    Refactor principal page to match nthe new BrickbroLogo component

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,13 @@
 'use client'; 
-import Image from "next/image";
-import SearchButtonAndInput from "../components/SearchButtonAndInput";
+import { SearchButtonAndInput, BrickBroLogo } from "../components/exports";
 
 
 export default function Home() {
 
   return (
     <>
-      <div className="flex flex-col gap-12 items-center space-y-4 md:space-x-4 md:space-y-0">
-        <Image
-          src="/logo_brickbro.png"
-          alt="Logo of the company BrickBro"
-          width={200}
-          height={100}
-        />
-
+      <div className="flex flex-col gap-12 items-center space-y-4 md:space-x-4 md:space-y-0"> 
+        <BrickBroLogo isCentered={true} />
         <SearchButtonAndInput />
       </div>
     </>
