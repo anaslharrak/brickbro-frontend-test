b6ed146873d0d715facb3538cd333d35f53ab5cd
Anas Lharrak <146816560+anaslharrak@users.noreply.github.com>
2024-07-14 19:03:50 +0200

    feat: :zap: added the MapComponent
    Added the map component to the page

---
+++ components/MapComponent.tsx
@@ -0,0 +1,26 @@
+import { API_KEY } from "@/app/constants"
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps"
+
+interface MapComponentProps {
+    position: {
+        lat: number,
+        lng: number
+    }
+}
+
+export default function MapComponent({position}: MapComponentProps) {
+    return (
+        <APIProvider apiKey={API_KEY}>
+            <Map
+            style={{width: '50vw', height: '80vh'}}
+            defaultCenter={{lat: position.lat, lng: position.lng}}
+            defaultZoom={18}
+            gestureHandling={'greedy'}
+            disableDefaultUI={true}
+            >
+                <Marker position={{ lat: position.lat, lng: position.lng }} />
+            </Map>
+        </APIProvider>
+        
+    )
+}
